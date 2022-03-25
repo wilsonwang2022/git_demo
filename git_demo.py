26cafb9ff078c4035a9c9daa56c1f728fb59d727
Wilson <102264105+wilsonwang2022@users.noreply.github.com>
2022-03-24 17:11:33 -0700

    Update git_demo.py

--- git_demo.py
+++ git_demo.py
@@ -1,3 +1,4 @@
 print('Commit 1: Git is a version controls technology installed on local machine!')
 print('Commit 2: GitHub is an online platform to host Git repositories')
 print('Commit 3: add some information in Github')
+print('Commit 4: Something changed')
